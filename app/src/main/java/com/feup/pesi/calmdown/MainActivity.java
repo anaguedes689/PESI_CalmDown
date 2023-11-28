67e98ab9c29f6b4c1a0212855698857d91a6a55b
up202308690 <up202308690@up.pt>
2023-11-28 15:53:52 +0000

    11commit - bia

--- app/src/main/java/com/feup/pesi/calmdown/MainActivity.java
+++ app/src/main/java/com/feup/pesi/calmdown/MainActivity.java
@@ -4,8 +4,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.feup.pesi.calmdown.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnStress, btnHrv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStress = findViewById(R.id.btnStress);
+        btnHrv = findViewById(R.id.btnHrv);
+
+        btnStress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Abrir StressActivity
+                Intent stressIntent = new Intent(MainActivity.this, StressActivity.class);
+                startActivity(stressIntent);
+            }
+        });
+
+        btnHrv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Abrir HrvActivity
+                Intent hrvIntent = new Intent(MainActivity.this, HrActivity.class);
+                startActivity(hrvIntent);
+            }
+        });
+    }
 }
+
